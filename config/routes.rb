a97c297cbd8ac37cf7eaafb75f345d720fd27b22
notmoebius <notmoebius@gmail.com>
2019-04-29 14:46:49 +0200

    url hidden welcom and update contact, team

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+  get 'welcome/:first_name', to: 'welcome#first_name'
+
   get '/team', to: 'static#team'
+
   get '/contact', to: 'static#contact'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
 end
