4d21722bda56f3bb9199dd46ee27e8b1487f074e
Labsss <alexandre.labonne@gmail.com>
2019-04-30 16:49:12 +0200

    stepppp

--- app/models/gossip.rb
+++ app/models/gossip.rb
@@ -3,6 +3,6 @@ class Gossip < ApplicationRecord
   belongs_to :tag_gossip, required: false
   has_many :tags, through: :tag_gossips
 
-  validates :title, presence: true, length: { minimum: 3, maximum: 14, message: "title need 3 car. or less than 14 car"} 
-  validates :content,presence: true
+  #validates :title, presence: true, length: { minimum: 3, maximum: 14, message: "title need 3 car. or less than 14 car"} 
+  #validates :content,presence: true
 end
